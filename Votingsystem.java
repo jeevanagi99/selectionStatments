060865843a89d53872443ec7dc2981c0dd9eca41
jeevanagi99 <89789326+jeevanagi99@users.noreply.github.com>
2021-09-07 12:04:58 +0530

    Add files via upload

---
+++ Votingsystem.java
@@ -0,0 +1,12 @@
+
+public class Votingsystem {
+
+	public static void main(String[] args) {
+		byte age=19;
+		if(age>=18);{
+		System.out.println("He is eligible for vote");
+		}
+
+	}
+
+}
---
+++ module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author arpita
+ *
+ */
+module InsideEclipse {
+}
